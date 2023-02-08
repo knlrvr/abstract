eea3cf80b59893441d4ae962e3dc25b54b44f32f
kane lariviere <91632194+knlrvr@users.noreply.github.com>
2023-02-08 07:56:33 -0500

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Hero() {
-    return (
+    return ( 
         <section className="h-screen">
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                 <Image  
