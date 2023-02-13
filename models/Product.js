680179cc1ed546df6473770da7f95466d113bb7d
kane lariviere <91632194+knlrvr@users.noreply.github.com>
2023-02-13 07:56:22 -0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,11 @@
 import { model, models, Schema } from 'mongoose';
 
 const ProductSchema = new Schema({
-    name: String,
-    description: String,
-    price: Number,
-    category: String,
-    picture: String,
+    name: String, 
+    description: String, 
+    price: Number, 
+    category: String, 
+    picture: String, 
 });
 
 const Product = models?.Product || model('Product', ProductSchema);
