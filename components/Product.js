5e6242a819eec9ade727d6a62becb66a4810f914
Kane <kanelariviere@outlook.com>
2023-04-10 16:08:39 -0400

    product styling

--- components/Product.js
+++ components/Product.js
@@ -23,14 +23,14 @@ export default function Product({_id,name,price,description,picture}) {
     setTimeout(setIsActive, 3000);
 
     return (
-        <div className="w-64 border p-4 rounded-lg shadow-md">
+        <div className="w-64 border p-4 rounded-md">
             <div className="p-2 rounded-xl h-48">
                 <Image 
                     src={picture} 
                     alt="product picture" 
                     width="1000"
                     height="0"
-                    className="rounded-xl"
+                    className="rounded-md"
                 />
             </div>
             <div className="mt-2">
@@ -41,11 +41,11 @@ export default function Product({_id,name,price,description,picture}) {
                 <div className="text-xl grow">${price}</div>
                 {isActive? 
                 <button onClick={() => { onClick(); setIsActive(!isActive)}} 
-                    className="bg-blue-400 py-2 px-5 rounded-full hover:bg-blue-300 text-white transition ease-in-out duration-400 hover:shadow-lg hover:shadow-gray-600 hover:shadow-rounded">
+                    className="bg-blue-400 py-2 px-5 rounded-full hover:bg-blue-300 text-white transition ease-in-out duration-400 hover:shadow-md hover:shadow-gray-600 hover:shadow-rounded">
                     <BsCheck2 />
                 </button> :
                 <button onClick={() => { onClick(); setIsActive(!isActive)}}
-                    className="bg-blue-400 py-2 px-5 rounded-full hover:bg-blue-300 text-white transition ease-in-out duration-400 hover:shadow-lg hover:shadow-gray-600 hover:shadow-rounded">
+                    className="bg-blue-400 py-2 px-5 rounded-full hover:bg-blue-300 text-white transition ease-in-out duration-400 hover:shadow-md hover:shadow-gray-600 hover:shadow-rounded">
                     <AiOutlinePlus />
                 </button>
                 }
