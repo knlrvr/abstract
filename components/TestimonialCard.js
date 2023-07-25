bbab5e9129f5feaa871f598e45b7d9af15b24ec7
Kane <kanelariviere@outlook.com>
2023-07-25 13:39:13 -0400

    small styling change on testimonial cards

--- components/TestimonialCard.js
+++ components/TestimonialCard.js
@@ -5,12 +5,14 @@ import { BsStar } from 'react-icons/bs'
 const TestimonialCard = ({ customer, quote, stars }) => {
   return (
     <div className="flex flex-col justify-between h-56 md:h-36 w-full">
-        <div className="flex text-3xl justify-center">
-            <BsStar /><BsStar /><BsStar /><BsStar /><BsStar />
+        <div className="flex flex-col space-y-2 text-3xl justify-center items-center">
+            <div className="flex">
+                <BsStar /><BsStar /><BsStar /><BsStar /><BsStar />
+            </div>
+            <span className="flex justify-center text-gray-400 text-sm">
+                {stars}
+            </span>
         </div>
-        <span className="flex justify-center text-gray-400 text-sm">
-            {stars}
-        </span>
         <p className="text-left md:text-center font-light">
             &quot; {quote} &quot;
         </p>
