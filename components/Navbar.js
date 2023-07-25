45b8fce26ec042ea2f2181faad31890653ba6c84
Kane <kanelariviere@outlook.com>
2023-07-25 09:23:38 -0400

    removed mr on nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,13 +10,13 @@ export default function Navbar() {
     const {selectedProducts} = useContext(ProductsContext);
 
     return (
-        <nav className="nav mt-2 py-2 px-4 md:px-8 lg:px-8 flex justify-between max-w-5xl mx-auto">
+        <nav className="nav mt-2 py-2 px-4 md:px-8 lg:px-8 flex items-center justify-between max-w-5xl mx-auto">
             <Link href="/">
             <h1 className="text-sm py-1 font-mono"> 
                 A b s t r a c t . 
             </h1>
             </Link>
-            <ul className="flex text-2xl space-x-6 mr-6 md:space-x-8 lg:space-x-10">
+            <ul className="flex text-2xl space-x-6 md:space-x-8 lg:space-x-10">
               <li>
                 <Link href='/login'>
                     <p className="text-xs md:text-sm py-1 border-b border-gray-600">
@@ -35,7 +35,7 @@ export default function Navbar() {
                 <Link href='/cart' className="flex items-center">
                     <BsCart 
                         className="mr-2" />
-                    <span className="cart text-blue-300 text-sm"> 
+                    <span className="cart text-blue-300 text-sm pt-1"> 
                         {selectedProducts.length}
                     </span>
                 </Link>
