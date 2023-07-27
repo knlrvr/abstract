a8998f764cb8051c53bcd49e4e2b8d103573b367
Kane <kanelariviere@outlook.com>
2023-07-27 09:41:20 -0400

    restyle login & signup pages

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import { useState } from 'react'
-import { BsLockFill } from "react-icons/bs";
+import { BsLock, BsLockFill } from "react-icons/bs";
 import Link from 'next/link'
 
 export default function Login() {
@@ -10,46 +10,54 @@ export default function Login() {
     return (
         <main>
             <Navbar />
-            <div className="text-center py-48">
-                <div className="flex justify-center mb-2 ">
-                    <BsLockFill />
-                </div>
-                <p className="mb-8">Secure Log In</p>
-                <div className="flex flex-col justify-center max-w-sm mx-auto p-2">
-                    <form action="/" 
-                        method="POST">
-                        <input
-                            type="text"
-                            name="email"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                            placeholder="Email Address"
-                            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                            placeholder="Password"
-                            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2 focus:outline-none"
-                        />
-                        <div className="text-xs flex justify-end mb-2 hover:text-blue-400 ">
-                            <Link href="/">
-                                <p>Forgot your password?</p>
-                            </Link>
+            <div className="max-w-5xl mx-auto">
+                <div className="h-screen flex justify-center items-center pb-12 px-2 md:px-6">
+                    <div className="border border-gray-300 bg-gray-50 w-full md:w-1/2 flex flex-col py-8 px-16 space-y-12 shadow-lg">
+                        
+                        <div className="flex items-center justify-between w-full">
+                            <BsLockFill />
+                            <span className="text-sm text-gray-400">Secure Log In</span>
                         </div>
-                        <button type="submit" className="bg-blue-300 px-5 py-2 rounded-xl font-thin text-white my-4 hover:bg-blue-400 hover:shadow-lg">
-                            Log In
-                        </button>
-                    </form>
-                    <div className="flex justify-center text-sm text-gray-500">
-                        <p>Don&apos;t have an account? <br />
-                            <Link href="/signup"
-                                className="text-gray-800 hover:text-blue-400">
-                                Sign Up!
-                            </Link>
-                        </p>
+
+                        <div className="">
+                            <form action="/"
+                                method="POST">
+                                <input
+                                    type="text"
+                                    name="email"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    placeholder="Email Address"
+                                    className="w-full bg-gray-100 rounded-lg px-4 py-2 mb-4" 
+                                />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    placeholder="Password"
+                                    className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2 focus:outline-none"
+                                />
+                                <div className="text-xs flex justify-end mb-2 hover:text-blue-400 ">
+                                    <Link href="/">
+                                        <p>Forgot your password?</p>
+                                    </Link>
+                                </div>
+                                <div className="flex justify-center">
+                                    <button type="submit" className="bg-blue-300 px-5 py-2 rounded-xl font-thin text-white my-4 hover:bg-blue-400 hover:shadow-lg">
+                                        Log In
+                                    </button>
+                                </div>
+                            </form>
+                            <div className="flex justify-center text-sm text-gray-500 pt-8">
+                                <p>Don&apos;t have an account? <br />
+                                    <Link href="/signup"
+                                        className="text-gray-800 hover:text-blue-400 flex justify-center">
+                                        Sign Up!
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- pages/signup.js
+++ pages/signup.js
@@ -13,65 +13,73 @@ export default function Signup() {
     return (
         <main>
             <Navbar />
-            <div className="text-center py-48">
-                <div className="flex justify-center mb-2 ">
-                    <BsLockFill />
-                </div>
-                <p className="mb-8">Secure Sign Up</p>
-                <div className="flex flex-col justify-center max-w-sm mx-auto p-2">
-                    <form action="/" 
-                        method="POST">
-                        <input
-                            type="text"
-                            name="name"
-                            value={firstName}
-                            onChange={e => setFirstName(e.target.value)}
-                            placeholder="First Name"
-                            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
-                        />
-                        <input
-                            type="text"
-                            name="name"
-                            value={lastName}
-                            onChange={e => setLastName(e.target.value)}
-                            placeholder="Last Name"
-                            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
-                        />
-                        <input
-                            type="text"
-                            name="email"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                            placeholder="Email Address"
-                            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                            placeholder="Password"
-                            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            value={confPassword}
-                            onChange={e => setConfPassword(e.target.value)}
-                            placeholder="Confirm Password"
-                            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
-                        />
-                        <button type="submit" className="bg-blue-300 px-5 py-2 rounded-xl font-thin text-white my-4 hover:bg-blue-400 hover:shadow-lg">
-                            Log In
-                        </button>
-                    </form>
-                    <div className="flex justify-center text-sm text-gray-500">
-                        <p>Already have an account? <br />
-                            <Link href="/login"
-                                className="text-gray-800 hover:text-blue-400">
-                                Log In!
-                            </Link>
-                        </p>
+            <div className="max-w-5xl mx-auto">
+                <div className="h-screen flex justify-center items-center pb-12 px-2 md:px-6">
+                    <div className="border border-gray-300 bg-gray-50 w-full md:w-1/2 flex flex-col py-8 px-16 space-y-12 shadow-lg">
+                        
+                        <div className="flex items-center justify-between w-full">
+                            <BsLockFill />
+                            <span className="text-sm text-gray-400">Secure Sign Up</span>
+                        </div>
+
+                        <div className="">
+                            <form action="/"
+                                method="POST">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={firstName}
+                                    onChange={e => setFirstName(e.target.value)}
+                                    placeholder="First Name"
+                                    className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
+                                />
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={lastName}
+                                    onChange={e => setLastName(e.target.value)}
+                                    placeholder="Last Name"
+                                    className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
+                                />
+                                <input
+                                    type="text"
+                                    name="email"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    placeholder="Email Address"
+                                    className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
+                                />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    placeholder="Password"
+                                    className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
+                                />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={confPassword}
+                                    onChange={e => setConfPassword(e.target.value)}
+                                    placeholder="Confirm Password"
+                                    className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-4 focus:outline-none"
+                                />
+                                <div className="flex justify-center">
+                                    <button type="submit" className="bg-blue-300 px-5 py-2 rounded-xl font-thin text-white my-4 hover:bg-blue-400 hover:shadow-lg">
+                                        Sign Up
+                                    </button>
+                                </div>
+                            </form>
+                            <div className="flex justify-center text-sm text-gray-500 pt-8">
+                                <p>Already have an account? <br />
+                                    <Link href="/login"
+                                        className="text-gray-800 hover:text-blue-400 flex justify-center">
+                                        Log In!
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
